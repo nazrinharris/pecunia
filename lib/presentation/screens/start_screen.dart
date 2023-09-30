956f23c692777a2bf3b580b6e3bc165cdf474631
nazrinharris <54095201+nazrinharris@users.noreply.github.com>
2023-09-30 15:28:45 -0400

    #103 Added a description text to StartScreen

--- lib/presentation/screens/start_screen.dart
+++ lib/presentation/screens/start_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:math' as math;
 
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gradient_animation_text/flutter_gradient_animation_text.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -22,87 +23,9 @@ class StartScreen extends HookWidget {
             const SizedBox(height: 10),
             WelcomeText(control),
             const Spacer(),
+            DescriptionText(control),
             AuthButtons(control),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class AuthButtons extends StatelessWidget {
-  const AuthButtons(this.control, {super.key});
-
-  final ValueNotifier<Control> control;
-
-  @override
-  Widget build(BuildContext context) {
-    return CustomAnimationBuilder(
-      control: control.value,
-      tween: Tween<Offset>(begin: const Offset(0, 500), end: Offset.zero),
-      curve: Curves.fastEaseInToSlowEaseOut,
-      duration: const Duration(milliseconds: 1000),
-      delay: const Duration(milliseconds: 900),
-      builder: (context, value, child) {
-        return Transform.translate(
-          offset: value,
-          child: child,
-        );
-      },
-      child: Container(
-        decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.primary.withOpacity(0.05),
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(34),
-              topRight: Radius.circular(34),
-            )),
-        child: Padding(
-          padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom + 34, top: 24),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.secondary),
-                  foregroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.onSecondary),
-                  splashFactory: InkRipple.splashFactory,
-                ),
-                onPressed: () {
-                  context.pushNamed('login');
-                },
-                child: Text('Login',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.onSecondary,
-                    )),
-              ),
-              const SizedBox(width: 14),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.secondary),
-                  foregroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.onSecondary),
-                  splashFactory: InkRipple.splashFactory,
-                ),
-                onPressed: () {
-                  context.pushNamed('register');
-                },
-                child: Text(
-                  'Register',
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.onSecondary,
-                  ),
-                ),
-              ),
-              const SizedBox(width: 14),
-              IconButton(
-                onPressed: () async {
-                  control.value = Control.playReverse;
-                  await Future<void>.delayed(const Duration(milliseconds: 2000));
-                  control.value = Control.play;
-                },
-                icon: const Icon(Icons.restart_alt_rounded),
-              )
-            ],
-          ),
         ),
       ),
     );
@@ -256,21 +179,6 @@ class WelcomeText extends StatelessWidget {
               ),
             );
           },
-          // child: ShaderMask(
-          //   shaderCallback: (bounds) => const LinearGradient(
-          //     colors: [Color(0xFF76FFF7), Color(0xFFF5FF7E), Color(0xFFF154FF)],
-          //     begin: Alignment.topLeft,
-          //     end: Alignment.bottomRight,
-          //   ).createShader(bounds),
-          //   child: Text(
-          //     'Pecunia',
-          //     style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-          //           fontWeight: FontWeight.bold,
-          //           fontSize: 64,
-          //           color: Colors.white,
-          //         ),
-          //   ),
-          // ),
           child: GradientAnimationText(
               text: Text('Pecunia',
                   style: Theme.of(context)
@@ -282,6 +190,141 @@ class WelcomeText extends StatelessWidget {
               transform: const GradientRotation(math.pi / 4)),
         ),
       ],
+    );
+  }
+}
+
+class DescriptionText extends StatelessWidget {
+  const DescriptionText(this.control, {super.key});
+
+  final ValueNotifier<Control> control;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomAnimationBuilder(
+      control: control.value,
+      tween: MovieTween()
+        ..tween(
+          'opacity',
+          Tween<double>(begin: 0, end: 1),
+          curve: Curves.easeOutCubic,
+          duration: const Duration(milliseconds: 700),
+        )
+        ..tween(
+          'position',
+          Tween<Offset>(begin: const Offset(0, 100), end: Offset.zero),
+          curve: Curves.easeOutCubic,
+          duration: const Duration(milliseconds: 700),
+        )
+        ..tween(
+          'scale',
+          Tween<double>(begin: 1.2, end: 1),
+          curve: Curves.easeOutCubic,
+          duration: const Duration(milliseconds: 500),
+        ),
+      duration: const Duration(milliseconds: 1000),
+      delay: const Duration(milliseconds: 1200),
+      builder: (context, value, child) {
+        return Opacity(
+          opacity: value.get('opacity'),
+          child: Transform.translate(
+            offset: value.get('position'),
+            child: Transform.scale(
+              scale: value.get('scale'),
+              child: child,
+            ),
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 84, vertical: 14),
+        child: Text(
+          'Say hello to stress-free money management',
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+        ),
+      ),
+    );
+  }
+}
+
+class AuthButtons extends StatelessWidget {
+  const AuthButtons(this.control, {super.key});
+
+  final ValueNotifier<Control> control;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomAnimationBuilder(
+      control: control.value,
+      tween: Tween<Offset>(begin: const Offset(0, 500), end: Offset.zero),
+      curve: Curves.fastEaseInToSlowEaseOut,
+      duration: const Duration(milliseconds: 1000),
+      delay: const Duration(milliseconds: 900),
+      builder: (context, value, child) {
+        return Transform.translate(
+          offset: value,
+          child: child,
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primary.withOpacity(0.05),
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(34),
+              topRight: Radius.circular(34),
+            )),
+        child: Padding(
+          padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom + 34, top: 24),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.secondary),
+                  foregroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.onSecondary),
+                  splashFactory: InkRipple.splashFactory,
+                ),
+                onPressed: () {
+                  context.pushNamed('login');
+                },
+                child: Text('Login',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onSecondary,
+                    )),
+              ),
+              const SizedBox(width: 14),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.secondary),
+                  foregroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.onSecondary),
+                  splashFactory: InkRipple.splashFactory,
+                ),
+                onPressed: () {
+                  context.pushNamed('register');
+                },
+                child: Text(
+                  'Register',
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.onSecondary,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 14),
+              IconButton(
+                onPressed: () async {
+                  control.value = Control.playReverse;
+                  await Future<void>.delayed(const Duration(milliseconds: 2000));
+                  control.value = Control.play;
+                },
+                icon: const Icon(Icons.restart_alt_rounded),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
